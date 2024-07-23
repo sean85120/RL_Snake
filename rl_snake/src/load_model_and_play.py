ff969c0d971451b005b0b0c4705ae9cbac1f5f42
Sean <91415054+sean85120@users.noreply.github.com>
2024-07-23 10:48:36 +0800

    modify the model_path as a arg in cli

--- rl_snake/src/load_model_and_play.py
+++ rl_snake/src/load_model_and_play.py
@@ -1,6 +1,20 @@
+import argparse
+
 from rl_snake.src.deep_q_network import DQNAgent, play_game_with_dqn_agent
 
 if __name__ == "__main__":
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description="Load a DQN model and play the game.")
+    parser.add_argument(
+        "--model_path", type=str, help="Path to the DQN model checkpoint"
+    )
+
+    # Parse command line arguments
+    args = parser.parse_args()
+
+    # Create DQNAgent and load the model
     agent = DQNAgent(state_size=12, action_size=4)
-    agent.load("./model_checkpoints/dqn_snake_model.pth")
+    agent.load(args.model_path)
+
+    # Play game with the loaded agent
     play_game_with_dqn_agent(agent)
